a84eea5059bc2713d3b9ba5b65618da7346a8952
serena3151 <77529411+serena3151@users.noreply.github.com>
2021-06-04 23:33:36 +1000

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,30 @@
+class launcher {
+    constructor(body,anchor){
+        var options = {
+            bodyA: body,
+            pointB: pointB,
+            stiffness:0.004,
+            length: 1
+        }
+        this.bodyA=body
+        this.pointB=anchor
+        this.launcher=Constraint.create(options)
+        World.add(world.this.launcher)
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+fly()
+{
+    this.launcher.bodyA = null;
+}
+}
+display()
+{
+    if(this.launcher.bodyA){
+        var pointA=this.bodyA.position;
+        var pointB=this.pointB
+        strokeWeight(3)
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
